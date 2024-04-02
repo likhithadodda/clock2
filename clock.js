5b02bca1e9a5c3104622fb6692044c9df1e3d4fe
likhitha <157030032+likhithadodda@users.noreply.github.com>
2024-04-02 12:30:30 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,16 @@
+function updateTime() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    var timeString = hours + ':' + minutes + ':' + seconds;
+
+    document.getElementById('time').innerHTML = timeString;
+}
+
+setInterval(updateTime, 1000); // Update time every second
